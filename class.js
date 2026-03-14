d2d1f9a244a1561a10bfd53d3cd1bb55ef92cba1
israMA00 <151969123+israMA00@users.noreply.github.com>
2026-03-14 12:45:25 -0600

    Change user's age

--- class.js
+++ class.js
@@ -3,6 +3,21 @@
 export class App {
     
     static exercise = () => {
+
+        const myMap = new Map([
+            ['user1', {name: 'isra', age: 41, id:123}],
+            ['user2', {name: 'jose', age: 23, id:890}],
+            ['user3', {name: 'maria', age: 15, id:456}],
+        ]);
+
+        if(myMap.has('user1')){
+            const getName = myMap.get('user1');
+            getName.age = 42;
+            myMap.set('user1', getName);
+        }
+
+        console.log(myMap);
+
        
     }
 }
