c15667b369c2077f72448bf486703e5c68455931
israMA00 <151969123+israMA00@users.noreply.github.com>
2026-03-14 14:02:01 -0600

    adding new Set

--- class.js
+++ class.js
@@ -4,10 +4,13 @@ export class App {
     
     static exercise = () => {
 
+        const mySet = new Set();
+
         const myMap = new Map([
             ['user1', {name: 'isra', age: 41, id:123}],
             ['user2', {name: 'jose', age: 23, id:890}],
             ['user3', {name: 'maria', age: 15, id:456}],
+            ['user4', {name: 'maria', age: 45, id:550}],
         ]);
 
         if(myMap.has('user1')){
@@ -16,8 +19,12 @@ export class App {
             myMap.set('user1', getName);
         }
 
-        console.log(myMap);
+        //adding user´s name to Set
+        myMap.values().forEach(user => {
+            mySet.add(user.name)
+        });
 
-       
+        console.log(mySet);
+    
     }
-}
+}clear
