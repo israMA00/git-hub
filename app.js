f1e47ea490025d59da8c0433fb0094592e36afc2
israMA00 <151969123+israMA00@users.noreply.github.com>
2026-03-15 11:48:51 -0600

    adding methods class

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ import { Account } from "./class.js";
 (function (){
 
    const user1 = new Account('isra', 1000);
+   user1.deposit(1000);
+   user1.withdraw(2000);
    console.log(user1.transactions);
+   console.log(user1.balance);
 
 })()
 
